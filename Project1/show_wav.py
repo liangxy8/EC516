676267c2b566167f4f01da4b5e61be9c40eadb8c
liangxy8 <liangxueyao1103@163.com>
2018-11-16 21:30:31 +0000

    upload

--- Project1/show_wav.py
+++ Project1/show_wav.py
@@ -4,14 +4,25 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # load the wav file and calculate the time 
-sample_rate, data = wavfile.read('may_i_help.wav')
-times = np.arange(len(data)/float(sample_rate))
-print(data.shape)
+sample_rate, data = wavfile.read('DSP.wav')
+times = np.arange(len(data))/float(sample_rate)
+#print(data.shape)
+#print(len(data))
+#print(data.size)
+
 # .ndim: Number of array dimensions
+# get the channels number
 if data.ndim > 1:
     sample_num, channel_num = data.shape
+    data = data.sum(axis = 1)
 else:
     channel_num = 1
     sample_num = data.shape[0]
 
-
+# show the waveform
+plt.plot(times, data/32768)
+plt.title('Speech Signal', fontsize = 18)
+plt.xlabel('time')
+plt.ylabel('amplitude')
+#plt.show()
+plt.savefig('show_signal.png')
